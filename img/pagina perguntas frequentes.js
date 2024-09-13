d9c636870fcec11d4adb6870b796cec39c0d8326
Maria Eduarda <mariaaraujo4830@gmail.com>
2024-09-13 18:30:38 -0300

    Projeto Integrador

---
+++ img/pagina perguntas frequentes.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.faq-question').forEach(item => {
+    item.addEventListener('click', () => {
+        const answer = item.nextElementSibling;
+        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    });
+});
