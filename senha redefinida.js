116c58e222c4cc43e8f021eaed66d183da64daef
Maria Eduarda <mariaaraujo4830@gmail.com>
2024-09-22 13:39:47 -0300

    Projeto Integrador

--- senha redefinida.js
+++ senha redefinida.js
@@ -3,8 +3,8 @@ function showConfirmation(message) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Redireciona para a página de login após 3 segundos
+    // Redireciona para a página de login após 5 segundos
     setTimeout(function() {
         window.location.href = 'pagina login.html';
-    }, 3000);
+    }, 5000);
 });
