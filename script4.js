595755bd2e4afb8fcc89bde1d2bd169a2852ce89
Maria Eduarda <mariaaraujo4830@gmail.com>
2024-09-25 10:55:59 -0300

    Projeto Integrador

--- script4.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const userInput = document.getElementById('userInput');
-
-    userInput.addEventListener('blur', function() {
-        alert('Você escreveu: ' + userInput.value);
-    });
-});
