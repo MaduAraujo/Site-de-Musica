a6402b13d60078fe356a0b952bd74ab165c3d75b
Maria Eduarda <mariaaraujo4830@gmail.com>
2024-09-25 10:52:42 -0300

    Projeto Integrador

---
+++ pagina podcast.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.list').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+});
