01c7a49311ebd9311a741fe34770f37c86acb774
Maria Eduarda <mariaaraujo4830@gmail.com>
2024-10-04 21:18:22 -0300

    Banco de Dados

---
+++ pagina sobre a empresa.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        var message = document.createElement('p');
+        message.innerText = 'Bem-vindo(a) à Aperte o Play!';
+        message.style.opacity = 0;
+        message.style.transition = 'opacity 2s';
+
+        // Adiciona o parágrafo antes do footer
+        var footer = document.querySelector('footer');
+        footer.parentNode.insertBefore(message, footer);
+
+        // Após adicionar, muda a opacidade para 1 para criar o efeito de desvanecimento
+        setTimeout(function() {
+            message.style.opacity = 1;
+        }, 100); // Pequeno atraso para que o efeito funcione
+    }, 3000); // 3000 milissegundos = 3 segundos
+});
+
+
